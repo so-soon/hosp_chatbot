eaa7f17c4bb55247bea2c85df47bb3627169e74d
so-soon <hyuk0610@gmail.com>
2020-02-13 18:26:36 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(userText)
+
+if __name__ == "__main__":
+    app.run()
